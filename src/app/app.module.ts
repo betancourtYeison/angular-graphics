5cfb282e6c7f8435161774f792ae14a46ca8bffc
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-05-13 16:22:02 -0500

    Create project graphics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { ChartsModule } from "ng2-charts";
+
+import { AppComponent } from "./app.component";
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LineChartComponent, BarChartComponent, DoughnutChartComponent, RadarChartComponent],
+  imports: [BrowserModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
